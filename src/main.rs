affadcd76980352c7346a762531d6d16b41c21e1
T.V Raman <tv.raman.tv@gmail.com>
2020-06-11 15:34:46 -0700

    runs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
-use std::io;
+use std::{io, process::exit};
+#[derive(Debug)]
+struct Game<'a> {
+    size: u32,
+    sticks: u32,
+    last_move: u32,
+    fibonacci: &'a Vec<u32>,
+}
 
 fn main() {
     println!("Welcome To Pick Up Sticks!");
     println!("How many sticks would you like to play with?");
     let game_size = read_number();
+    if game_size <= 2 {
+        println!("Game size of 2  or less is too small");
+        exit(1);
+    }
     let fibs = gen_fibs_upto(game_size);
-    let mut sticks = game_size;
-    if !fib_p(&fibs, game_size) {
+    println!("Fibonacci: {:?}", fibs);
+    let game = Game {
+        size: game_size,
+        sticks: game_size,
+        fibonacci: &fibs,
+        last_move: 0,
+    };
+    println!("{:?}", game);
+    if !fib_p(&fibs, game.size) {
         println!("I play first.");
-        my_move(&fibs, sticks);
+        my_move(&game);
+    }
+    while game.sticks > 0 {
+        your_move(&game);
+        my_move(&game);
     }
 }
 
-fn my_move(fibs: &Vec<u32>, sticks: u32) {}
+fn my_move(game: &Game) {
+    game.sticks;
+}
 
-fn your_move(fibs: &Vec<u32>, sticks: u32) {}
-
-fn valid_move_p(fibs: &Vec<u32>, sticks: u32) -> bool {
-    true
+fn your_move(game: &Game) {
+    game.sticks;
 }
 
 fn gen_fibs_upto(game_size: u32) -> Vec<u32> {
     let mut fibs = vec![1, 2];
-    if game_size <= 2 {
-        return fibs;
-    }
     let mut size = fibs.len();
     while fibs[size - 1] < game_size {
         fibs.push(fibs[size - 1] + fibs[size - 2]);
